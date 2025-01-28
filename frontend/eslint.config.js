4b7f08b36903643e7c39317b4704104001816990
Janika Savela <janika_s@hotmail.com>
2025-01-28 21:41:56 +0200

    add pipeline

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh' // Lisäämämme plugin
 
 export default [
   {
@@ -15,7 +14,6 @@ export default [
         sourceType: 'module', // Käytetään module-lähdettä
         ecmaFeatures: { jsx: true }, // Mahdollistetaan JSX
       },
-      parser: 'babel-eslint', // Määritellään parseri (babel-eslint)
     },
     settings: {
       react: { version: 'detect' }, // Reactin versio automaattisesti tunnistetaan
@@ -23,21 +21,16 @@ export default [
     plugins: {
       react, // Reactin plugin
       'react-hooks': reactHooks, // React Hooksin plugin
-      'react-refresh': reactRefresh, // React Refreshin plugin
     },
-    extends: [
-      'eslint:recommended', // Suositellut ESLintin säännöt
-      'plugin:react/recommended', // Suositellut Reactin säännöt
-      'plugin:react-hooks/recommended', // Suositellut React Hooksin säännöt
-    ],
     rules: {
+      // ESLintin ja Reactin suositellut säännöt
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
       'react/jsx-no-target-blank': 'warn', // Varoitus target="_blank" käytöstä ilman rel="noopener noreferrer"
       'react/prop-types': 0, // Poistetaan prop-types tarkistukset (voi lisätä jos tarvitset)
       'react/react-in-jsx-scope': 'off', // Vite ei tarvitse tätä
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ], // React Refresh sääntö
     },
   },
 ]
